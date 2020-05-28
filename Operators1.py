795a0dd71fa938b47c8ae72328a81fb773d70bc5
Justin-Paul-2001 <66091941+Justin-Paul-2001@users.noreply.github.com>
2020-05-29 02:46:44 +0530

    Create Operators1.py

---
+++ Operators1.py
@@ -0,0 +1,32 @@
+#assignment operator
+x = 5 
+print(x)
+
+# x = x + 3
+x += 3 
+print(x)
+
+# x = x - 5
+x -= 5 
+print(x)
+
+# x = x * 4
+x *= 4 
+print(x)
+
+# x = x / 3
+x /= 3 
+print(x)
+
+# x = x % 2 -> Remainder Operator -> Gives Remainder of the Division
+x %= 2 
+print(x)
+
+# x = x // 3 -> Floor Division Operator -> Only Gives Integer value of Division
+x = 19
+x //= 3 
+print(x)
+
+# x = x ** 4 -> exponential operator -> 2 raise to 3 {2 ** 3 or 2 ^ 3}
+x **= 4 
+print(x)
