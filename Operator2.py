f1c7bd29be5eb3c154af52d1c74f4d50c0c74662
Justin-Paul-2001 <66091941+Justin-Paul-2001@users.noreply.github.com>
2020-05-29 02:40:25 +0530

    Create Operator2.py

---
+++ Operator2.py
@@ -0,0 +1,28 @@
+# Comparison Operators
+x = 5
+y = 7
+print(x == y)
+print(x != y)
+print(x <= y)
+print(x >= y)
+print(x < y)
+print(x > y) 
+
+# Logical Operators
+a = 5
+b = 7
+c = "abcefghijkm"
+print(a == b and a != c)
+print(a == b or a != c)
+print(not a == c)
+
+# Identity Operator
+d = [1,2,3]
+e = [4,5,6]
+print(d is e)
+print(d is not e)
+
+# Membership Operator
+f = [1,4,5,9]
+print(4 in f)
+print(8 not in f)
