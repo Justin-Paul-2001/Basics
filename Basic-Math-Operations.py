112026d01fa0f2758e17a97a50d73031cbfe41b1
Justin Paul Kolengadan <66091941+Justin-Paul-2001@users.noreply.github.com>
2020-05-29 03:08:06 +0530

    Create Basic-Math-Operations.py

---
+++ Basic-Math-Operations.py
@@ -0,0 +1,16 @@
+a = 14
+b = 3
+c = a + b
+d = a - b
+e = a * b # Multiplication
+f = a / b
+g = a ** b # Power / Exponentiation / 'a' raised to 'b'
+h = a / /b # Floor Division -> Only Integer Value of Division is Returned
+i = a % b  # Remainder -> Only Remainder of The Division is Returned
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
