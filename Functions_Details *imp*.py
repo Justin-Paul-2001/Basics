af09a0a39b5613b789976bbf0f5acd8fdbc280fb
Justin Paul Kolengadan <66091941+Justin-Paul-2001@users.noreply.github.com>
2020-05-31 22:21:32 +0530

    Create Functions_Details *imp*.py

---
+++ Functions_Details *imp*.py
@@ -0,0 +1,41 @@
+# Defining and Calling A Function:
+
+def number_squared(number): # Defining A Function
+                            # Note : Try always to return
+                            # A value from the function instead
+                            # of printing something
+
+                            # Here 'number' is argument
+    return number ** 2
+
+print(number_squared(5)) # Calling the Function
+                         # Here '5' is parameter
+
+# Finding The Hypotenuse of Right Angle Triangle:
+
+def side_squared(side):
+    return side ** 2
+
+def hypotenuse_right_angle_traingle(base,perpendicular):
+    return (side_squared(base) + side_squared(perpendicular))** .5
+
+print(hypotenuse_right_angle_traingle(3,4))
+
+# Program Using Arbitrary Argument:
+
+# When we want to pass arguments as a list use {*arg_name}
+
+def getName(*name):  # When we don't know number of
+                     # arguments we use {*} before the
+                     # argument and use indexing while
+                     # writing the body of function.
+    return ("The Name is " + name[2])
+
+print(getName("Justin","Paul","Suja","Alan","Meressa","Seby","Swapna"))
+
+# When we want to pass arguments as a dictionary use {**arg_name}
+
+def getName1(**name):
+    return ("The Name Is "+ name["name5"])
+
+print(getName1(name1 = "Justin",name2 = "Paul",name3 = "Suja",name4 = "Alan",name5 = "Meressa",name6 = "Swapna"))
