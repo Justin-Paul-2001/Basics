fd4417354ff1cb120504e3379e61341ac0180e04
Justin Paul Kolengadan <66091941+Justin-Paul-2001@users.noreply.github.com>
2020-06-14 00:20:13 +0530

    Create Math_Library-Details.py

---
+++ Math_Library-Details.py
@@ -0,0 +1,40 @@
+# Programs On Math Library / Module ->
+
+import math as mt
+
+max_element = max(20,30,40,50,25,55,65,35,45,60,15) # Returns Max Element
+min_element = min(20,30,40,50,25,55,65,35,45,60,15) # Returns Min Element
+gcd_value = mt.gcd(12,36)              # Returns GCD -> Greatest Common Divisor of Two Numbers.
+cosine_radian_value = mt.cos(2)        # Returns Cosine Value in Radians
+sine_radian_value = mt.sin(2)          # Returns Sine Value in Radians
+absolute_element = abs(-9.823)         # Returns Absolute Value |modulus|
+power_operation = pow(4,3)             # Returns 4 ^ 3 i.e 4 raised to 3
+                                       # pow(base , exponent)
+
+square_root = mt.sqrt(16)              # Returns Square Root of a Number
+nearest_upper_integer = mt.ceil(14.4)  # Returns Nearest Greater Integer
+nearest_lower_integer = mt.floor(14.4) # Returns Nearest Smallest Integer
+pi_value = mt.pi                       # Assigns a value of 3.1415926...
+factorial_value = mt.factorial(6)      # Returns Factorial Value
+log_base_10_value = mt.log10(100)      # Returns Logarithm with base 10
+log_base_2_value = mt.log2(100)        # Returns Logarithm with base 2
+natural_log = mt.log(100)              # Returns Natural Logarithm Value
+exponential_value = mt.exp(10)         # Returns Exponential Value
+
+print(max_element)
+print(min_element)
+print(gcd_value)
+print(cosine_radian_value)
+print(sine_radian_value)
+print(absolute_element)
+print(power_operation)
+print(square_root)
+print(nearest_upper_integer)
+print(nearest_lower_integer)
+print(pi_value)
+print(round(pi_value,2)) # round(float , no. of decimals)
+print(factorial_value)
+print(log_base_10_value)
+print(log_base_2_value)
+print(natural_log)
+print(exponential_value)
